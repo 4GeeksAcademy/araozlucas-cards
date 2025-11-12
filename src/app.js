bb6b7742b9c419e78a8564c306185b9fbdec35ef
Pimple <araozlucas@gmail.com>
2025-11-12 04:41:12 +0000

    Second commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import "bootstrap";
 import "./style.css";
 
+let palo = ['heart','spade','club','diamond'];
+let numbers = ['1','2','3','4','5','6','7','8','9','10','J','Q','K'];
+let cards = [];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function RandomElement(array) {
+  return [Math.floor( Math.random()* array.length)];
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.getElementById("card").innerHTML =
+    `${RandomElement(numbers)}`;
+    //write your code here
 };
