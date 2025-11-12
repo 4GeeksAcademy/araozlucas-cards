824efbf4d5974f02ebd958b07b3228e6e630a979
Pimple <araozlucas@gmail.com>
2025-11-12 22:03:44 +0000

    last commit

--- src/app.js
+++ src/app.js
@@ -1,16 +1,34 @@
 import "bootstrap";
 import "./style.css";
 
-let palo = ['heart','spade','club','diamond'];
-let numbers = ['1','2','3','4','5','6','7','8','9','10','J','Q','K'];
-let cards = [];
+let palos = ['heart','spade','club','diamond'];
+let numbers = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
 
 function RandomElement(array) {
-  return [Math.floor( Math.random()* array.length)];
+  return array[Math.floor( Math.random()* array.length)];
 }
 
-window.onload = () => {
-  document.getElementById("card").innerHTML =
-    `${RandomElement(numbers)}`;
-    //write your code here
+const palo = RandomElement(palos)
+const numero = RandomElement(numbers)
+
+const simbols = {
+  heart: '♥',
+  spade: '♠',
+  club: '♣',
+  diamond: '♦'
 };
+
+window.onload = () => {
+  const numE1 = document.getElementById("number");
+  const topE1 = document.getElementById("paloSuperior");
+  const bottomE1 = document.getElementById("paloInferior");
+  const cardE1 = document.querySelector(".card");
+
+  numE1.textContent = numero;
+  topE1.textContent = simbols[palo];
+  bottomE1.textContent = simbols[palo];
+
+  cardE1.classList.add(palo);
+};
+
+
